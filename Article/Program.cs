24156925464cf609238c6bcc930a3e1ddc7e43d1
Seyed Mehdi Setodegun <seyedmehdisetodegun@gmail.com>
2024-02-27 17:01:40 +0330

    feat(UserInformationModel): add UserInformationModel

--- Article/Program.cs
+++ Article/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +25,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
