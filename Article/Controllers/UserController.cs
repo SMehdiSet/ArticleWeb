4c3808d70b0947e31989e9b365d2d48fdd3aa6db
Seyed Mehdi Setodegun <seyedmehdisetodegun@gmail.com>
2024-03-05 17:48:46 +0330

    feat(UserControler DbContext): add DbContext to UserControler.cs and add Register functionality.

--- Article/Controllers/UserController.cs
+++ Article/Controllers/UserController.cs
@@ -1,10 +1,18 @@
-﻿using ArticleWeb.ViewModels;
+﻿using Article.Data;
+using ArticleWeb.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Article.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public UserController(ApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,10 +27,23 @@ namespace Article.Controllers
         [HttpPost]
         public IActionResult Register(UserInformationModel User)
         {
+            if (ModelState.IsValid)
+            {
+
+                _applicationDbContext.UserInformationDb.Add(User);
+                _applicationDbContext.SaveChanges();
+
+                return RedirectToAction("Acount");
+            }
             return View();
         }
 
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Acount()
         {
             return View();
         }
